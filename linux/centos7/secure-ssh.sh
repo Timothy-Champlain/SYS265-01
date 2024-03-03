e250d890b2058074b7b1344f095e42f29fbad72e
Timothy-Champlain <timothy.price@mymail.champlain.edu>
2024-03-03 16:44:09 -0500

    Wrote a secure ssh file, I think

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author gmcyber
+#creates a new ssh user using the $l parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE GOES HERE"
